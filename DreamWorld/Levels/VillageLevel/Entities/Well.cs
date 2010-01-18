6282b5257b0d7506fbd562ce09a19a9829947f64
Ruurtjan Pul <ruurtjan@gmail.com>
2010-01-18 12:57:09 +0000

    Added collisionData for the Well

--- DreamWorld/Levels/VillageLevel/Entities/Well.cs
+++ DreamWorld/Levels/VillageLevel/Entities/Well.cs
@@ -1,4 +1,5 @@
 ﻿using DreamWorld.Entities;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace DreamWorld.Levels.VillageLevel.Entities
@@ -10,6 +11,131 @@ namespace DreamWorld.Levels.VillageLevel.Entities
         {
             Model = GameScreen.Content.Load<Model>(@"Models\Village\Well");
             base.LoadContent();
+        }
+
+        /// <summary>
+        /// Creates all needed object information for the JigLibX physics simulator.
+        /// </summary>
+        /// <param name="body">Returns Body.</param>
+        /// <param name="skin">Returns CollisionSkin.</param>
+        /// <param name="centerOfMass">Returns the center of mass wich is usefull for drawing objects.</param>
+        protected override void GetPhysicsInformation(out JigLibX.Physics.Body body, out JigLibX.Collision.CollisionSkin skin, out Vector3 centerOfMass)
+        {
+            #region Header
+            // Variables
+            JigLibX.Collision.MaterialProperties materialProperties;
+            JigLibX.Geometry.PrimitiveProperties primitiveProperties;
+            JigLibX.Geometry.Box box;
+
+            #region Mass Variables
+            float mass;
+            Matrix inertiaTensor;
+            Matrix inertiaTensorCoM;
+            #endregion
+
+            // Create Skin & Body
+            body = new JigLibX.Physics.Body();
+            skin = new JigLibX.Collision.CollisionSkin(body);
+            body.CollisionSkin = skin;
+            #endregion
+
+            #region Primitive Properties
+            primitiveProperties = new JigLibX.Geometry.PrimitiveProperties();
+            primitiveProperties.MassDistribution = JigLibX.Geometry.PrimitiveProperties.MassDistributionEnum.Solid;
+            primitiveProperties.MassType = JigLibX.Geometry.PrimitiveProperties.MassTypeEnum.Mass;
+            primitiveProperties.MassOrDensity = 0.001f;
+            #endregion
+
+            #region Primitive 0
+            // MaterialProperties:
+            materialProperties = new JigLibX.Collision.MaterialProperties();
+            materialProperties.StaticRoughness = 0.5f;
+            materialProperties.DynamicRoughness = 0.35f;
+            materialProperties.Elasticity = 0.25f;
+
+            // Primitive:
+            box = new JigLibX.Geometry.Box(Vector3.Transform(new Vector3(-2.629032f, 47.00237f, -29.2319f), Matrix.CreateScale(Scale)),
+                Matrix.CreateFromQuaternion(new Quaternion(0f, 0f, 0f, 0.9999999f)),
+                Vector3.Transform(new Vector3(8.996243f, 10.58278f, 58.63294f), Matrix.CreateScale(Scale)));
+
+            skin.AddPrimitive(box, materialProperties);
+            #endregion
+
+            #region Primitive 1
+            // MaterialProperties:
+            materialProperties = new JigLibX.Collision.MaterialProperties();
+            materialProperties.StaticRoughness = 0.5f;
+            materialProperties.DynamicRoughness = 0.35f;
+            materialProperties.Elasticity = 0.25f;
+
+            // Primitive:
+            box = new JigLibX.Geometry.Box(Vector3.Transform(new Vector3(-17.8207f, -5.695952f, -37.60003f), Matrix.CreateScale(Scale)),
+                Matrix.CreateFromQuaternion(new Quaternion(0f, 0f, 0f, 0.9999999f)),
+                Vector3.Transform(new Vector3(33.88134f, 39.81426f, 74.74816f), Matrix.CreateScale(Scale)));
+
+            skin.AddPrimitive(box, materialProperties);
+            #endregion
+
+            #region Primitive 2
+            // MaterialProperties:
+            materialProperties = new JigLibX.Collision.MaterialProperties();
+            materialProperties.StaticRoughness = 0.5f;
+            materialProperties.DynamicRoughness = 0.35f;
+            materialProperties.Elasticity = 0.25f;
+
+            // Primitive:
+            box = new JigLibX.Geometry.Box(Vector3.Transform(new Vector3(-38.74889f, -10.7307f, -20.45407f), Matrix.CreateScale(Scale)),
+                Matrix.CreateFromQuaternion(new Quaternion(0f, 0f, 0f, 0.9999999f)),
+                Vector3.Transform(new Vector3(76.78346f, 42.86791f, 39.49059f), Matrix.CreateScale(Scale)));
+
+            skin.AddPrimitive(box, materialProperties);
+            #endregion
+
+            #region Primitive 3
+            // MaterialProperties:
+            materialProperties = new JigLibX.Collision.MaterialProperties();
+            materialProperties.StaticRoughness = 0.5f;
+            materialProperties.DynamicRoughness = 0.35f;
+            materialProperties.Elasticity = 0.25f;
+
+            // Primitive:
+            box = new JigLibX.Geometry.Box(Vector3.Transform(new Vector3(15.71199f, -8.477726f, -38.29074f), Matrix.CreateScale(Scale)),
+                Matrix.CreateFromQuaternion(new Quaternion(0f, -0.3701087f, 0f, 0.9289884f)),
+                Vector3.Transform(new Vector3(29.00214f, 39.9366f, 77.41502f), Matrix.CreateScale(Scale)));
+
+            skin.AddPrimitive(box, materialProperties);
+            #endregion
+
+            #region Primitive 4
+            // MaterialProperties:
+            materialProperties = new JigLibX.Collision.MaterialProperties();
+            materialProperties.StaticRoughness = 0.5f;
+            materialProperties.DynamicRoughness = 0.35f;
+            materialProperties.Elasticity = 0.25f;
+
+            // Primitive:
+            box = new JigLibX.Geometry.Box(Vector3.Transform(new Vector3(-37.15203f, -4.989683f, -20.78619f), Matrix.CreateScale(Scale)),
+                Matrix.CreateFromQuaternion(new Quaternion(0f, 0.3692448f, 0f, 0.9293321f)),
+                Vector3.Transform(new Vector3(26.6786f, 36.61219f, 81.224f), Matrix.CreateScale(Scale)));
+
+            skin.AddPrimitive(box, materialProperties);
+            #endregion
+
+            #region Footer
+            // Extract Mass Properties
+            skin.GetMassProperties(primitiveProperties, out mass, out centerOfMass, out inertiaTensor, out inertiaTensorCoM);
+
+            // Set Mass Properties
+            body.BodyInertia = inertiaTensorCoM;
+            body.Mass = mass;
+
+            // Sync Body & Skin
+            body.MoveTo(Vector3.Zero, Matrix.Identity);
+            skin.ApplyLocalTransform(new JigLibX.Math.Transform(Vector3.Zero, Matrix.CreateScale(1, 1, -1)));
+
+            // Enable Body
+            body.EnableBody();
+            #endregion
         }
     }
 }
