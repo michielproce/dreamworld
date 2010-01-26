12d3db39d93bac8b9a2d14e98fcbcb90aa2d359d
Michiel Proce <mail@michielproce.nl>
2010-01-26 22:06:03 +0000

    Brand new Morwir + including cane.

---
+++ DreamWorld/Levels/VillageLevel/Entities/MorwirCane.cs
@@ -0,0 +1,15 @@
+﻿using DreamWorld.Entities;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class MorwirCane : Entity
+    {
+        public static bool ListInEditor = true;
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\Cane");
+            base.LoadContent();
+        }
+    }
+}
