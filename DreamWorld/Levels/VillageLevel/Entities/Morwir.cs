edc60a58e07e93764d4943b1d99052a8f83fc82c
Michiel Proce <mail@michielproce.nl>
2010-01-26 14:00:45 +0000

    Morwir ingame (zodattie op de juiste plek gezet kan worden)

---
+++ DreamWorld/Levels/VillageLevel/Entities/Morwir.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DreamWorld.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class Morwir : Entity
+    {
+        public static bool ListInEditor = true;
+
+        public override void Initialize()
+        {
+            Animation.InitialClip = "Standing";
+            Animation.Speed = 1.0f;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\Morwir");
+            base.LoadContent();
+        }
+    }
+}
