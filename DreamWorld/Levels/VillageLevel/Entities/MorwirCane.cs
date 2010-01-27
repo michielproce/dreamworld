1c6a3f5ecd4f09efb43b98e9fecf6f039a9f05d6
Michiel Proce <mail@michielproce.nl>
2010-01-27 10:18:47 +0000

    Added animated morwir's cane

--- DreamWorld/Levels/VillageLevel/Entities/MorwirCane.cs
+++ DreamWorld/Levels/VillageLevel/Entities/MorwirCane.cs
@@ -6,6 +6,14 @@ namespace DreamWorld.Levels.VillageLevel.Entities
     public class MorwirCane : Entity
     {
         public static bool ListInEditor = true;
+
+        public override void Initialize()
+        {
+            Animation.InitialClip = "Standing";
+            Animation.Speed = 1.0f;
+            base.Initialize();
+        }
+
         protected override void LoadContent()
         {
             Model = GameScreen.Content.Load<Model>(@"Models\Village\Cane");
