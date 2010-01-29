d285e6249a2fddfe7e4f011c0a42135c27703ae0
Ruurtjan Pul <ruurtjan@gmail.com>
2010-01-29 13:40:39 +0000

    Added Stable.

---
+++ DreamWorld/Levels/VillageLevel/Entities/Stable.cs
@@ -0,0 +1,16 @@
+﻿using DreamWorld.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class Stable : Entity
+    {
+        public static bool ListInEditor = true;
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\Stable");
+            base.LoadContent();
+        }
+    }
+}
