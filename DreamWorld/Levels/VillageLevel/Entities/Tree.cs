27891e5020bdb98854763eed8f078a74e5b560a8
Michiel Proce <mail@michielproce.nl>
2010-04-11 11:45:02 +0000

    Added Statue, Tree and TreeTrunk. TODO: Physics Geometry.

---
+++ DreamWorld/Levels/VillageLevel/Entities/Tree.cs
@@ -0,0 +1,15 @@
+﻿using DreamWorld.Entities;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class Tree : Entity
+    {
+        public static bool ListInEditor = true;
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\Tree");
+            base.LoadContent();
+        }
+    }
+}
