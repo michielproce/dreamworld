d2d5785c7fbc92d22ee661684e21b3e0daa4bb76
Michiel Proce <mail@michielproce.nl>
2010-04-22 10:16:12 +0000

    Added StableTrashed and Skull

---
+++ DreamWorld/Levels/VillageLevel/Entities/Skull.cs
@@ -0,0 +1,15 @@
+﻿using DreamWorld.Entities;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class Skull : Entity
+    {
+        public static bool ListInEditor = true;
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\Skull");
+            base.LoadContent();
+        }
+    }
+}
---
+++ DreamWorld/Levels/VillageLevel/Entities/StableTrashed.cs
@@ -0,0 +1,16 @@
+﻿using DreamWorld.Entities;
+using DreamWorld.ScreenManagement.Screens;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamWorld.Levels.VillageLevel.Entities
+{
+    public class StableTrashed : Entity
+    {
+        public static bool ListInEditor = true;
+        protected override void LoadContent()
+        {
+            Model = GameScreen.Content.Load<Model>(@"Models\Village\StableTrashed");
+            base.LoadContent();
+        }
+    }
+}
